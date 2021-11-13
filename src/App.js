557d7a6cf08c9cbd5ebf2766b72d6f2f94736bb5
Socolov Alexandr <82725768+assassin0210@users.noreply.github.com>
2021-11-13 20:08:25 +0200

    asd

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from "react";
 import {Home} from "./pages/Home";
+import {GlobalStyle} from "./components/GlobolStyle";
 
 
 function App() {
@@ -7,6 +8,7 @@ function App() {
     return (
         <div className="App">
             <Home/>
+            <GlobalStyle/>
         </div>
     );
 }
